461941193befa1eba42ea5f6aea68baa0e6a7da5
krishn-maloo <98144812+krishn-maloo@users.noreply.github.com>
2023-09-11 12:43:33 +0530

    Update crc.c

--- crc.c
+++ crc.c
@@ -1,58 +1,34 @@
-#include <stdio.h>
-#include <string.h>
+def bin_xor(result,a,b):
+    for i in range(16):
+        result[i] = '0' if a[i] == b[i] else '1'
+    result[15] ='\0'
 
-// CRC-CCITT polynomial: x^16 + x^12 + x^5 + 1 (0x1021 in hexadecimal or 1000000100000101 in binary)
-#define CRC_POLY_BINARY 0b1000000100000101
+def cal(data,crc_poly):
+    crc = '0' * 16
+    for byte in data:
+        for _ in range(8):
+            msb = crc[0]
+            crc = crc[1:] + '0'
+            if msb == '1':
+                temp = ['0'] *16
+                bin_xor(temp,crc,crc_poly)
+                crc = ''.join(temp)
+        crc = crc[:15] + ('1' if byte=='1' else '0')
+    return crc
 
-// Function to calculate CRC-CCITT checksum
-unsigned short calculateCRC(const char *data, int length) {
-    unsigned short crc = 0xFFFF; // Initial value
-    for (int i = 0; i < length; i++) {
-        crc ^= (unsigned short)data[i] << 8;
-        for (int j = 0; j < 8; j++) {
-            if (crc & 0x8000)
-                crc = (crc << 1) ^ CRC_POLY_BINARY;
-            else
-                crc <<= 1;
-        }
-    }
-    return crc;
-}
+def main():
+    data = input("enter the data in binary")
+    crc_poly = bin(int('0X11021',16))[2:]
+    print(crc_poly)
+    check_sum = cal(data,crc_poly)
+    print(check_sum)
+    code = data+check_sum
+    print(code)
+    rec = input("enter the checksum u got")
+    if code== rec:
+        print("sahi hai")
+    else:
+        print("galat hai")
 
-int main() {
-    char data[100]; // Replace with your actual data
-    printf("Enter data in binary: ");
-    scanf("%s", data);
 
-    int dataLength = strlen(data);
-    unsigned short checksum = calculateCRC(data, dataLength);
-
-    printf("Calculated CRC in binary: ");
-    for (int i = 15; i >= 0; i--) {
-        printf("%d", (checksum >> i) & 0x01);
-    }
-    printf("\n");
-
-    // Simulating error by changing a bit
-    // data[2] ^= 0x01; // Uncomment this line to introduce an error
-
-    // Verify the received data
-    unsigned short receivedChecksum;
-    printf("Enter received CRC in binary: ");
-    char receivedChecksumBinary[17];
-    scanf("%16s", receivedChecksumBinary);
-
-    receivedChecksum = 0;
-    for (int i = 0; i < 16; i++) {
-        receivedChecksum <<= 1;
-        receivedChecksum |= receivedChecksumBinary[i] - '0';
-    }
-
-    if (receivedChecksum == checksum) {
-        printf("Data is error-free.\n");
-    } else {
-        printf("Data contains errors.\n");
-    }
-
-    return 0;
-}
+main()
